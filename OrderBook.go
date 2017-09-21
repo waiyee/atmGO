38fd8ee2a3b777ff677c3ac5d33b6e8cc7d9a4da
waiyee <waiyee422@hotmail.com>
2017-09-21 09:17:17 +0800

    bug fixed

--- OrderBook.go
+++ OrderBook.go
@@ -342,7 +342,7 @@ func refreshOrder(){
 							v.Buy.Quantity = result.Quantity
 							v.Buy.Total = result.Price
 							v.Buy.Status = "bought"
-							t, err2 := time.Parse("2006-01-02T15:04:05.00", result.Closed)
+							t, err2 := time.Parse("2006-01-02T15:04:05.0000", result.Closed)
 							if err2 != nil {
 								e := session.DB("v2").C("ErrorLog").With(session)
 								e.Insert(&db.ErrorLog{Description: "Re-prase time error", Error: err2.Error(), Time: time.Now()})
@@ -359,7 +359,7 @@ func refreshOrder(){
 							v.Sell.Quantity = result.Quantity
 							v.Sell.Total = result.Price
 							v.Sell.Status = "sold"
-							t, err2 := time.Parse("2006-01-02T15:04:05.00", result.Closed)
+							t, err2 := time.Parse("2006-01-02T15:04:05.0000", result.Closed)
 							if err2 != nil {
 								e := session.DB("v2").C("ErrorLog").With(session)
 								e.Insert(&db.ErrorLog{Description: "Re-prase time error", Error: err2.Error(), Time: time.Now()})
